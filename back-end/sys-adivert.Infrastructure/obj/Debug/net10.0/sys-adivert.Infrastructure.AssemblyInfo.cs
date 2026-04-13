c93b9f82e048132362cb14450f06a55256558fe6
Miguel Passos <gibertoni.passos@gmail.com>
2026-04-13 16:12:08 -0300

    fazendo pull

--- back-end/sys-adivert.Infrastructure/obj/Debug/net10.0/sys-adivert.Infrastructure.AssemblyInfo.cs
+++ back-end/sys-adivert.Infrastructure/obj/Debug/net10.0/sys-adivert.Infrastructure.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("sys-adivert.Infrastructure")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bce6d4331ea807c5c9659c5b84befb6c3f064b14")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c64d751218a812e585ac10b631caa61d845814c6")]
 [assembly: System.Reflection.AssemblyProductAttribute("sys-adivert.Infrastructure")]
 [assembly: System.Reflection.AssemblyTitleAttribute("sys-adivert.Infrastructure")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
