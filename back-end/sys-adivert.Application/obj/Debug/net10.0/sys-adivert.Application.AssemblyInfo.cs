a89a176429122329ba3d4529807a12a4700970f3
Miguel Passos <gibertoni.passos@gmail.com>
2026-04-23 22:55:58 -0300

    arrumando bugs

--- back-end/sys-adivert.Application/obj/Debug/net10.0/sys-adivert.Application.AssemblyInfo.cs
+++ back-end/sys-adivert.Application/obj/Debug/net10.0/sys-adivert.Application.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("sys-adivert.Application")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+325773eb0380e7c4bd4a5433b4997528bb9a5af1")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+643aa9a525948ce3f8126c02c3ce776acf3e773c")]
 [assembly: System.Reflection.AssemblyProductAttribute("sys-adivert.Application")]
 [assembly: System.Reflection.AssemblyTitleAttribute("sys-adivert.Application")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
